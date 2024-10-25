c0fbab8de253e7d2190dc2b9670353a70cfe8d1b
Thais Menezes <thais.mas@hotmail.com>
2024-10-25 10:49:30 -0300

    Initial Commit

---
+++ App_Swiftui/App_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_SwiftuiApp.swift
+//  App_Swiftui
+//
+//  Created by Thais Souza on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct App_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
